bc5afc2cf16f6630fb335e89e5bea53de2e169b9
PyoneerO <31238935+PyoneerO@users.noreply.github.com>
2018-07-26 11:31:25 +0200

    Update Illumina_paired_end_fastq_subsampling.sh

--- Illumina_paired_end_fastq_subsampling.sh
+++ Illumina_paired_end_fastq_subsampling.sh
@@ -8,4 +8,4 @@ parallel -j $cores '\
 paste {}_L001_R1_001.fastq {}_L001_R2_001.fastq |\
 awk '\''{printf("%s",$0); n++; if(n%4==0) {printf("\n");} else {printf("\t");}}'\'' |\
 shuf | head -10000 |\
-awk -F"\t" '\''{print $1"\n"$3"\n"$5"\n"$7 > {}"_L001_R1_001.fastq; print $2"\n"$4"\n"$6"\n"$8 > {}"_L001_R2_001.fastq}'\'''
+awk -F"\t" '\''{print $1"\n"$3"\n"$5"\n"$7 > "{}_sub_L001_R1_001.fastq"; print $2"\n"$4"\n"$6"\n"$8 > "{}_sub_L001_R2_001.fastq"}'\'''

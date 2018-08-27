7413ebf8349eb2ee84a210ec7ffc3164c948f665
PyoneerO <31238935+PyoneerO@users.noreply.github.com>
2018-08-27 15:40:39 +0200

    Update Illumina_paired_end_fastq_gunzip_parallel.sh

--- Illumina_paired_end_fastq_gunzip_parallel.sh
+++ Illumina_paired_end_fastq_gunzip_parallel.sh
@@ -1,10 +1,10 @@
-input_dir="test_fastqs"
-output_dir="test_output"
-cores=30 
+input_dir="../q-zip2_full"
+output_dir="."
+threads=30 
 
 for i in $input_dir/*R1_001.fastq.gz; do 
   samplename=`echo $i | awk '{gsub("_L001_R1_001.fastq.gz","",$0); print}'`;echo -e $samplename
 done \
-| parallel -j $cores '\
+| parallel -j $threads '\
 gunzip -c {}_L001_R1_001.fastq.gz > '$output_dir'/`basename {}`_L001_R1_001.fastq; \
 gunzip -c {}_L001_R2_001.fastq.gz > '$output_dir'/`basename {}`_L001_R2_001.fastq'

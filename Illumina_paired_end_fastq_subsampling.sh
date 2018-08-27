994ea905faccaf47e4c302b8c2b442fe7a8f1e8b
PyoneerO <31238935+PyoneerO@users.noreply.github.com>
2018-08-27 15:39:14 +0200

    Update Illumina_paired_end_fastq_subsampling.sh

--- Illumina_paired_end_fastq_subsampling.sh
+++ Illumina_paired_end_fastq_subsampling.sh
@@ -1,10 +1,10 @@
-input_dir="test_fastqs"
-output_dir="test_output"
-cores=30 
+input_dir="../../common/test_data/"
+output_dir="."
+threads=10 
 for i in $input_dir/*R1_001.fastq; do 
   samplename=`echo $i | awk '{gsub("_L001_R1_001.fastq","",$0); print}'`;echo -e $samplename
 done |\
-parallel -j $cores '\
+parallel -j $threads '\
 paste {}_L001_R1_001.fastq {}_L001_R2_001.fastq |\
 awk '\''{printf("%s",$0); n++; if(n%4==0) {printf("\n");} else {printf("\t");}}'\'' |\
 shuf | head -10000 |\
